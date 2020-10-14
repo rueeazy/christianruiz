8df8f4f02ce9422f7a32fbec7b6d9060485f77de
Christian Ruiz <santacruz.crm@gmail.com>
2020-10-14 16:49:57 -0700

    Updates

---
+++ precache-manifest.9a9b4e781e091700fb2f1ef69fac24e3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3320af34448ba5b8e8233a33b55539d",
+    "url": "/christianruiz/index.html"
+  },
+  {
+    "revision": "aaeced1c43738031bae8",
+    "url": "/christianruiz/static/css/main.c7168dae.chunk.css"
+  },
+  {
+    "revision": "02ff998d89ed1142a42e",
+    "url": "/christianruiz/static/js/2.be4fc5bf.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/christianruiz/static/js/2.be4fc5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaeced1c43738031bae8",
+    "url": "/christianruiz/static/js/main.99a1ca54.chunk.js"
+  },
+  {
+    "revision": "e42596209ce81f79b2da",
+    "url": "/christianruiz/static/js/runtime-main.50e4d4a7.js"
+  },
+  {
+    "revision": "808d74065dd8a77b6629ee33fd61910b",
+    "url": "/christianruiz/static/media/battleship.808d7406.png"
+  },
+  {
+    "revision": "738b5f7e8593815b7997e02fd0c0f275",
+    "url": "/christianruiz/static/media/chess.738b5f7e.png"
+  },
+  {
+    "revision": "667f89aa638c0dc1d0873953d7555980",
+    "url": "/christianruiz/static/media/memory.667f89aa.png"
+  },
+  {
+    "revision": "d0f61104f9f0ac3d2b0cec3dcdc3bcb8",
+    "url": "/christianruiz/static/media/resume.d0f61104.pdf"
+  },
+  {
+    "revision": "11b96ccbd27451aafb45287c055f001d",
+    "url": "/christianruiz/static/media/shopping.11b96ccb.png"
+  },
+  {
+    "revision": "b31ca42836787f1b69c7e67f3a2b2151",
+    "url": "/christianruiz/static/media/tic-tac-toe.b31ca428.png"
+  },
+  {
+    "revision": "fc41eb230966d0a659688df1d609c377",
+    "url": "/christianruiz/static/media/todo.fc41eb23.png"
+  },
+  {
+    "revision": "342702f433346e16c0954c4840a938b9",
+    "url": "/christianruiz/static/media/waldo.342702f4.png"
+  },
+  {
+    "revision": "a8d2febdef7e89738b5548c5c6c5e0ec",
+    "url": "/christianruiz/static/media/weather.a8d2febd.png"
+  }
+]);
